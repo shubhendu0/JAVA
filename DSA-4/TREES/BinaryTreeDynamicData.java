930746783c5ef6e67bc8dcd0473ea1aa28f08d9d
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2022-06-13 23:00:30 +0530

    Update BinaryTreeDynamicData.java

--- DSA-4/TREES/BinaryTreeDynamicData.java
+++ DSA-4/TREES/BinaryTreeDynamicData.java
@@ -116,23 +116,3 @@ public class BinaryTreeDynamicData {
     }
 }
   
-// public void insert(Node node, int value) {
-    //     if (value < node.value) { 
-    //       if (node.left != null) { 
-    //         insert(node.left, value); 
-    //       } 
-    //       else { 
-    //         System.out.println("Inserted " + value + " to left of " + node.value); 
-    //         node.left = new Node(value);
-    //       } 
-    //     } 
-    //     else if (value > node.value) {
-    //       if (node.right != null) {
-    //         insert(node.right, value);
-    //       } 
-    //       else {
-    //         System.out.println("Inserted " + value + " to right of "+ node.value);
-    //         node.right = new Node(value);
-    //       }
-    //     }
-    //   }
